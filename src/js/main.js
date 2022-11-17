8438e483ceb5b180a40c145fd935b1ba40a86da3
SkylarWang <ziyiziyi@bu.edu>
2022-11-17 00:54:22 -0500

    Add name&score&collision( collision is buggy now

--- src/js/main.js
+++ src/js/main.js
@@ -13,12 +13,12 @@
  *
  */
 
-import * as titleScene from './scenes/titleScene.js';
-import * as playScene from './scenes/playScene.js';
-import * as onboardingScene from './scenes/onboardingScene.js';
-import * as winningScene from './scenes/winningScene.js';
-import * as losingScene from './scenes/losingScene.js';
-import * as scoreScene from './scenes/scoreScene.js';
+import * as titleScene from "./scenes/titleScene.js";
+import * as playScene from "./scenes/playScene.js";
+import * as onboardingScene from "./scenes/onboardingScene.js";
+import * as winningScene from "./scenes/winningScene.js";
+import * as losingScene from "./scenes/losingScene.js";
+import * as scoreScene from "./scenes/scoreScene.js";
 
 export let shared, my, guests;
 export let images = {};
@@ -31,116 +31,116 @@ let currentScene; // the scene being displayed
 
 // all the available scenes
 export let scenes = {
-	title: titleScene, // Scene 1: Game Tile & Cover Page
-	onboard: onboardingScene, // Scene 2: Onboarding
-	play: playScene, // Scene 3: Main Game Page
-	win: winningScene,
-	lose: losingScene,
-	score: scoreScene,
+  title: titleScene, // Scene 1: Game Tile & Cover Page
+  onboard: onboardingScene, // Scene 2: Onboarding
+  play: playScene, // Scene 3: Main Game Page
+  win: winningScene,
+  lose: losingScene,
+  score: scoreScene,
 };
 
 // p5.js auto detects your setup() and draw() before "installing" itself but
 // since this code is a module the functions aren't global. This creates aliases
 // of the p5 functions on window, so p5.js can find them
 Object.assign(window, {
-	preload,
-	draw,
-	setup,
-	mousePressed,
-	keyPressed,
+  preload,
+  draw,
+  setup,
+  mousePressed,
+  keyPressed,
 });
 
 function preload() {
-	/* ----------------------------- p5.party setup ----------------------------- */
-	partyConnect(
-		'wss://deepstream-server-1.herokuapp.com',
-		'DDF_wk2-1115_bysimone',
-		'main'
-	);
-	shared = partyLoadShared('shared'); // create shared data
-	my = partyLoadMyShared(); // create local data of my object
-	guests = partyLoadGuestShareds(); // create data of all players
+  /* ----------------------------- p5.party setup ----------------------------- */
+  partyConnect(
+    "wss://deepstream-server-1.herokuapp.com",
+    "DDF_wk2-1115_bysimone",
+    "main"
+  );
+  shared = partyLoadShared("shared", { bombs: [] }); // create shared data
+  my = partyLoadMyShared(); // create local data of my object
+  guests = partyLoadGuestShareds(); // create data of all players
 
-	/* ----------------------------- load assets ----------------------------- */
-	// load image
-	images.bomb = loadImage('../../assets/images/bomb.png');
-	images.map = loadImage('../../assets/images/map.png');
+  /* ----------------------------- load assets ----------------------------- */
+  // load image
+  images.bomb = loadImage("../../assets/images/bomb.png");
+  images.map = loadImage("../../assets/images/map.png");
 
-	// load avatar
-	images.avatar = [];
-	for (let i = 0; i < PLAYER_NUM; i++) {
-		images.avatar[i] = {};
-		let j = 0;
-		images.avatar[i].left = loadImage(
-			'../../assets/images/character' + j + '/left.png'
-		);
-		images.avatar[i].right = loadImage(
-			'../../assets/images/character' + j + '/right.png'
-		);
-		images.avatar[i].up = loadImage(
-			'../../assets/images/character' + j + '/back.png'
-		);
-		images.avatar[i].down = loadImage(
-			'../../assets/images/character' + j + '/front.png'
-		);
-		images.avatar[i].default = images.avatar[i].left;
-	}
+  // load avatar
+  images.avatar = [];
+  for (let i = 0; i < PLAYER_NUM; i++) {
+    images.avatar[i] = {};
+    let j = 0;
+    images.avatar[i].left = loadImage(
+      "../../assets/images/character" + j + "/left.png"
+    );
+    images.avatar[i].right = loadImage(
+      "../../assets/images/character" + j + "/right.png"
+    );
+    images.avatar[i].up = loadImage(
+      "../../assets/images/character" + j + "/back.png"
+    );
+    images.avatar[i].down = loadImage(
+      "../../assets/images/character" + j + "/front.png"
+    );
+    images.avatar[i].default = images.avatar[i].left;
+  }
 
-	/* ----------------------------- load setup config from other scenes ----------------------------- */
-	Object.values(scenes).forEach((scene) => scene.preload?.());
+  /* ----------------------------- load setup config from other scenes ----------------------------- */
+  Object.values(scenes).forEach((scene) => scene.preload?.());
 }
 
 function setup() {
-	createCanvas(800, 600).parent('canvas-wrap');
+  createCanvas(800, 600).parent("canvas-wrap");
 
-	// set shared variables
-	if (partyIsHost()) {
-		// when the player is the first player of the game
-		partySetShared(shared, {
-			bombs: [],
-			bombSize: 20,
-			loadingTime: 10,
-			timer: 90,
-		});
-	}
-	// assign local data to my object
-	partySetShared(my, {
-		role: 'observer',
-		xPos: random(width / 2),
-		yPos: 300,
-		direction: 'left',
-		side: 'left',
-	});
+  // set shared variables
+  if (partyIsHost()) {
+    // when the player is the first player of the game
+    partySetShared(shared, {
+      bombs: [],
+      bombSize: 20,
+      loadingTime: 10,
+      timer: 90,
+    });
+  }
+  // assign local data to my object
+  partySetShared(my, {
+    role: "observer",
+    xPos: random(width / 2),
+    yPos: 300,
+    direction: "left",
+    side: "left",
+  });
 
-	Object.values(scenes).forEach((scene) => scene.setup?.());
-	changeScene(scenes.title);
+  Object.values(scenes).forEach((scene) => scene.setup?.());
+  changeScene(scenes.title);
 }
 
 function draw() {
-	// update
-	currentScene?.update?.();
-	// draw
-	currentScene?.draw?.();
+  // update
+  currentScene?.update?.();
+  // draw
+  currentScene?.draw?.();
 }
 
 function mousePressed() {
-	currentScene?.mousePressed?.();
+  currentScene?.mousePressed?.();
 }
 
 function keyPressed() {
-	currentScene?.keyPressed?.();
+  currentScene?.keyPressed?.();
 }
 
 export function changeScene(newScene) {
-	if (!newScene) {
-		console.error('newScene not provided');
-		return;
-	}
-	if (newScene === currentScene) {
-		console.error('newScene is already currentScene');
-		return;
-	}
-	currentScene?.leave?.();
-	currentScene = newScene;
-	currentScene.enter?.();
+  if (!newScene) {
+    console.error("newScene not provided");
+    return;
+  }
+  if (newScene === currentScene) {
+    console.error("newScene is already currentScene");
+    return;
+  }
+  currentScene?.leave?.();
+  currentScene = newScene;
+  currentScene.enter?.();
 }
--- src/js/scenes/playScene.js
+++ src/js/scenes/playScene.js
@@ -1,178 +1,226 @@
 import {
-	changeScene,
-	scenes,
-	shared,
-	my,
-	guests,
-	images,
-	PLAYER_NUM,
-} from '../main.js';
+  changeScene,
+  scenes,
+  shared,
+  my,
+  guests,
+  images,
+  PLAYER_NUM,
+} from "../main.js";
+
+//player names
+const names = [
+  "Wind",
+  "Snow",
+  "Light",
+  "Smoke",
+  "Fog",
+  "Cloud",
+  "Bird",
+  "Plane",
+  "Rain",
+  "Rainbow",
+  "Angel",
+  "Pilot",
+];
+
+function pick(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
 export function setup() {
-	my.bomb = {
-		xPos: 0, // position of ball
-		yPos: 0,
-		dX: 0, // velocity of ball
-		dY: 0,
-		r: 20, // radius or
-		active: true, // if the ball is visible
-		floor: 0, // where the ball drops
-		age: 0, // length
-	};
+  my.bomb = {
+    xPos: 0, // position of ball
+    yPos: 0,
+    dX: 0, // velocity of ball
+    dY: 0,
+    r: 20, // radius or
+    active: true, // if the ball is visible
+    floor: 0, // where the ball drops
+    age: 0, // length
+  };
 
-	assignPosition();
-	assignPlayers();
+  my.name = pick(names);
+  my.life = 10;
+
+  assignPosition();
+  assignPlayers();
 }
 
 export function enter() {}
 
 export function update() {
-	updateBomb(my.bomb, my);
+  updateBomb(my.bomb, my);
+
+  //hit(shared.bombs);
+  hit(my.bomb);
 }
 
 export function draw() {
-	background(0);
-	noStroke();
-	image(images.map, 0, 0, width, height);
+  background(0);
+  noStroke();
+  image(images.map, 0, 0, width, height);
 
-	// draw me
-	image(images.avatar[0].default, my.xPos, my.yPos, 30, 30);
-	// draw bombs
-	drawBomb(my.bomb);
-	// draw all players
-	drawPlayers();
-	// move me
-	moveCharacter();
+  //shared.bombs.forEach(drawBomb);
+  //draw names
+  for (let i = 0; i < guests.length; i++) {
+    textSize(13);
+    fill(60, 14, 12);
+    //how to change my name into a different color?
+    text(guests[i].name + ":" + guests[i].life, 100 * i + 100, 20);
+  }
 
-	/* ------------------------------- UI/UX ------------------------------- */
-	// draw info
-	push();
-	fill('white');
-	text('play scene', 10, 20);
-	pop();
+  // draw me
+  image(images.avatar[0].default, my.xPos, my.yPos, 30, 30);
+  // draw bombs
+  drawBomb(my.bomb);
+  // draw all players
+  drawPlayers();
+  // move me
+  moveCharacter();
+  //decrease life when getting hit
+
+  /* ------------------------------- UI/UX ------------------------------- */
+  // draw info
+  push();
+  fill("white");
+  text("play scene", 10, 20);
+  pop();
 }
 
 /* --------------------------------- Bomb ---------------------------------- */
 
 function startBomb(bomb, player) {
-	bomb.xPos = player.xPos;
-	bomb.yPos = player.yPos;
-	bomb.floor = player.yPos;
-	bomb.dX = 5;
-	bomb.dY = -10;
-	bomb.active = true;
-	bomb.age = 0;
-	console.log(bomb, player);
+  bomb.xPos = player.xPos;
+  bomb.yPos = player.yPos;
+  bomb.floor = player.yPos;
+  bomb.dX = 5;
+  bomb.dY = -10;
+  bomb.active = true;
+  bomb.age = 0;
+  console.log(bomb, player);
 }
 
 function updateBomb(bomb, player) {
-	if (!bomb.active) return;
+  if (!bomb.active) return;
 
-	if (player.direction == 'right') {
-		bomb.xPos += bomb.dX; // apply velocity to x
-	} else if (player.direction == 'left') {
-		bomb.xPos -= bomb.dX; // apply velocity to x
-	}
-	bomb.yPos += bomb.dY; // apply velocity to y
-	bomb.dY += 0.6; // apply gravity
+  if (player.direction == "right") {
+    bomb.xPos += bomb.dX; // apply velocity to x
+  } else if (player.direction == "left") {
+    bomb.xPos -= bomb.dX; // apply velocity to x
+  }
+  bomb.yPos += bomb.dY; // apply velocity to y
+  bomb.dY += 0.6; // apply gravity
 
-	// bounce off bottom of screen
-	// if (bomb.y + bomb.r > height) {
-	//   bomb.y = height - bomb.r;
-	//   bomb.dY = -abs(bomb.dY);
-	// }
+  // bounce off bottom of screen
+  // if (bomb.y + bomb.r > height) {
+  //   bomb.y = height - bomb.r;
+  //   bomb.dY = -abs(bomb.dY);
+  // }
 
-	if (bomb.yPos > bomb.floor) {
-		bomb.dY = -abs(bomb.dY);
-		// hurt enemies
-		// bomb.active = false;
-	}
+  if (bomb.yPos > bomb.floor) {
+    bomb.dY = -abs(bomb.dY);
+    // hurt enemies
+    // bomb.active = false;
+  }
 
-	// age bomb
-	bomb.age++;
-	if (bomb.age > 35) {
-		// hurt enemies
-		bomb.active = false;
-	}
-
-	//console.log(bomb, player);
+  // age bomb
+  bomb.age++;
+  for (const guest of guests) {
+    if (
+      bomb.age > 35
+      // bomb.age > 35 ||
+      // dist(bomb.xPos, bomb.yPos, guest.xPos, guest.yPos) <= 0
+    ) {
+      // hurt enemies
+      bomb.active = false;
+    }
+  }
 }
 
+//console.log(bomb, player);
+
 function drawBomb(bomb) {
-	// render
-	if (!bomb.active) return;
-	fill('black');
-	ellipse(bomb.xPos, bomb.yPos, bomb.r, bomb.r);
+  // render
+  if (!bomb.active) return;
+  fill("black");
+  ellipse(bomb.xPos, bomb.yPos, bomb.r, bomb.r);
+}
+
+function hit(bomb) {
+  for (const guest of guests) {
+    if (dist(bomb.xPos, bomb.yPos, guest.xPos, guest.yPos) < 50) guest.life--;
+  }
 }
 
 /* --------------------------------- Player Assignment ---------------------------------- */
 
 function assignPosition() {
-	let playerSum = PLAYER_NUM > guests.length ? guests.length : PLAYER_NUM;
-	for (let i = 0; i < playerSum; i++) {
-		guests[i].xPos = random(width);
-		guests[i].yPos = random(height);
-	}
+  let playerSum = PLAYER_NUM > guests.length ? guests.length : PLAYER_NUM;
+  for (let i = 0; i < playerSum; i++) {
+    guests[i].xPos = random(width);
+    guests[i].yPos = random(height);
+  }
 }
 
 function assignPlayers() {
-	let playerSum = PLAYER_NUM > guests.length ? guests.length : PLAYER_NUM;
-	for (let i = 0; i < playerSum; i++) {
-		if (!guests.find((p) => p.role === 'player')) {
-			// find the first observer
-			const o = guests.find((p) => p.role === 'observer');
-			// if thats me, take the role
-			if (o === my) {
-				o.role = 'player';
-			}
-		}
-	}
+  let playerSum = PLAYER_NUM > guests.length ? guests.length : PLAYER_NUM;
+  for (let i = 0; i < playerSum; i++) {
+    if (!guests.find((p) => p.role === "player")) {
+      // find the first observer
+      const o = guests.find((p) => p.role === "observer");
+      // if thats me, take the role
+      if (o === my) {
+        o.role = "player";
+      }
+    }
+  }
 }
 function drawPlayers() {
-	let playerSum = PLAYER_NUM > guests.length ? guests.length : PLAYER_NUM;
-	for (let i = 0; i < playerSum; i++) {
-		image(
-			images.avatar[i][guests[i].direction],
-			guests[i].xPos,
-			guests[i].yPos,
-			30,
-			30
-		);
-	}
+  let playerSum = PLAYER_NUM > guests.length ? guests.length : PLAYER_NUM;
+  for (let i = 0; i < playerSum; i++) {
+    image(
+      images.avatar[i][guests[i].direction],
+      guests[i].xPos,
+      guests[i].yPos,
+      30,
+      30
+    );
+  }
 }
 
 /* --------------------------------- Input ---------------------------------- */
 
 function moveCharacter() {
-	// up: w, up arrow
-	if (keyIsDown(87) /*w*/ || keyIsDown(38) /*up*/) {
-		if (my.yPos > 0) my.yPos--;
-		my.direction = 'up';
-	}
-	// down: s, down arrow
-	if (keyIsDown(83) /*s*/ || keyIsDown(40) /*left*/) {
-		if (my.yPos < height) my.yPos++;
-		my.direction = 'down';
-	}
-	// left: a, left arrow
-	if (keyIsDown(65) /*a*/ || keyIsDown(37) /*left*/) {
-		if (my.xPos > 0) my.xPos--;
-		my.direction = 'left';
-	}
-	// right: d, right arrow
-	if (keyIsDown(68) /*d*/ || keyIsDown(39) /*right*/) {
-		if (my.xPos < width) my.xPos++;
-		my.direction = 'right';
-	}
+  // up: w, up arrow
+  if (keyIsDown(87) /*w*/ || keyIsDown(38) /*up*/) {
+    if (my.yPos > 0) my.yPos--;
+    my.direction = "up";
+  }
+  // down: s, down arrow
+  if (keyIsDown(83) /*s*/ || keyIsDown(40) /*left*/) {
+    if (my.yPos < height) my.yPos++;
+    my.direction = "down";
+  }
+  // left: a, left arrow
+  if (keyIsDown(65) /*a*/ || keyIsDown(37) /*left*/) {
+    if (my.xPos > 0) my.xPos--;
+    my.direction = "left";
+  }
+  // right: d, right arrow
+  if (keyIsDown(68) /*d*/ || keyIsDown(39) /*right*/) {
+    if (my.xPos < width) my.xPos++;
+    my.direction = "right";
+  }
 }
 
 export function mousePressed() {
-	changeScene(scenes.title);
+  changeScene(scenes.title);
 }
 
 export function keyPressed() {
-	if (keyCode === 32) {
-		startBomb(my.bomb, my);
-		console.log('space is pressed');
-	}
+  if (keyCode === 32) {
+    startBomb(my.bomb, my);
+    console.log("space is pressed");
+  }
 }
